a34e6a5f7ca8798144c3075e085629e460c474b0
Christophe Jauffret <christophe.jauffret@nutanix.com>
2022-08-01 20:05:49 +0200

    fix copyright (#94)

--- test/e2e/multi_nic_test.go
+++ test/e2e/multi_nic_test.go
@@ -2,7 +2,7 @@
 // +build e2e
 
 /*
-Copyright 2021.
+Copyright 2022 Nutanix
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
