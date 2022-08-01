5564b16bc07770ce13c0232ba221acccff1ba707
Yannick Struyf <yannick.struyf@nutanix.com>
2022-08-01 16:30:58 +0200

    read e2e parameters from e2econfig instead of env var (#93)

--- test/e2e/nutanix_client.go
+++ test/e2e/nutanix_client.go
@@ -21,14 +21,24 @@ package e2e
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"strconv"
 
 	prismGoClient "github.com/nutanix-cloud-native/prism-go-client/pkg/nutanix"
 	prismGoClientV3 "github.com/nutanix-cloud-native/prism-go-client/pkg/nutanix/v3"
 	. "github.com/onsi/gomega"
+	"sigs.k8s.io/cluster-api/test/framework/clusterctl"
 
 	nutanixClientHelper "github.com/nutanix-cloud-native/cluster-api-provider-nutanix/pkg/client"
+)
+
+const (
+	nutanixEndpointVarKey = "NUTANIX_ENDPOINT"
+	nutanixPortVarKey     = "NUTANIX_PORT"
+	nutanixInsecureVarKey = "NUTANIX_INSECURE"
+	nutanixUsernameVarKey = "NUTANIX_USER"
+	nutanixPasswordVarKey = "NUTANIX_PASSWORD"
 )
 
 var (
@@ -38,43 +48,74 @@ var (
 )
 
 func init() {
-	flag.StringVar(&nutanixEndpoint, "e2e.nutanixEndpoint", os.Getenv("NUTANIX_ENDPOINT"), "the Nutanix Prism Central used for e2e tests")
-	flag.StringVar(&nutanixPort, "e2e.nutanixPort", os.Getenv("NUTANIX_PORT"), "the Nutanix Prism Central port used for e2e tests")
-	flag.StringVar(&nutanixInsecure, "e2e.nutanixInsecure", os.Getenv("NUTANIX_INSECURE"), "Ignore certificate checks for e2e tests")
+	flag.StringVar(&nutanixEndpoint, "e2e.nutanixEndpoint", os.Getenv(nutanixEndpointVarKey), "the Nutanix Prism Central used for e2e tests")
+	flag.StringVar(&nutanixPort, "e2e.nutanixPort", os.Getenv(nutanixPortVarKey), "the Nutanix Prism Central port used for e2e tests")
+	flag.StringVar(&nutanixInsecure, "e2e.nutanixInsecure", os.Getenv(nutanixInsecureVarKey), "Ignore certificate checks for e2e tests")
 }
 
-type nutanixCredentials struct {
-	nutanixUsername string
-	nutanixPassword string
+func fetchCredentialParameter(key string, config clusterctl.E2EConfig, allowEmpty bool) string {
+	value := os.Getenv(key)
+
+	if value == "" && config.HasVariable(key) {
+		value = config.GetVariable(key)
+	}
+
+	if allowEmpty && value == "" {
+		return value
+	}
+	Expect(value).ToNot(BeEmpty(), "expected parameter %s to be set", key)
+	return value
 }
 
-func getNutanixCredentialsFromEnvironment() nutanixCredentials {
-	nutanixUsername := os.Getenv(nutanixUserKey)
-	Expect(nutanixUsername).ToNot(BeEmpty(), "expected environment variable %s to be set", nutanixUserKey)
-	nutanixPassword := os.Getenv(nutanixPasswordKey)
-	Expect(nutanixPassword).ToNot(BeEmpty(), "expected environment variable %s to be set", nutanixPasswordKey)
-	return nutanixCredentials{
-		nutanixUsername: nutanixUsername,
-		nutanixPassword: nutanixPassword,
+type baseAuthCredentials struct {
+	username string
+	password string
+}
+
+func getBaseAuthCredentials(e2eConfig clusterctl.E2EConfig) baseAuthCredentials {
+	return baseAuthCredentials{
+		username: fetchCredentialParameter(nutanixUsernameVarKey, e2eConfig, false),
+		password: fetchCredentialParameter(nutanixPasswordVarKey, e2eConfig, false),
 	}
 }
 
-func initNutanixClient() (*prismGoClientV3.Client, error) {
-	insecureBool, err := strconv.ParseBool(nutanixInsecure)
+func getNutanixCredentials(e2eConfig clusterctl.E2EConfig) (*prismGoClient.Credentials, error) {
+	up := getBaseAuthCredentials(e2eConfig)
+	if nutanixEndpoint == "" {
+		nutanixEndpoint = fetchCredentialParameter(nutanixEndpointVarKey, e2eConfig, false)
+	}
+	if nutanixPort == "" {
+		nutanixPort = fetchCredentialParameter(nutanixPortVarKey, e2eConfig, true)
+	}
+	if nutanixInsecure == "" {
+		nutanixInsecure = fetchCredentialParameter(nutanixInsecureVarKey, e2eConfig, true)
+	}
+
+	var insecureBool bool
+	var err error
+
+	if nutanixInsecure != "" {
+		insecureBool, err = strconv.ParseBool(nutanixInsecure)
+		if err != nil {
+			return nil, fmt.Errorf("unable to convert value for environment variable %s to bool: %v", nutanixInsecureVarKey, err)
+		}
+	}
+	return &prismGoClient.Credentials{
+		Insecure: insecureBool,
+		Port:     nutanixPort,
+		Endpoint: nutanixEndpoint,
+		Username: up.username,
+		Password: up.password,
+	}, nil
+}
+
+func initNutanixClient(e2eConfig clusterctl.E2EConfig) (*prismGoClientV3.Client, error) {
+	creds, err := getNutanixCredentials(e2eConfig)
 	if err != nil {
 		return nil, err
 	}
 
-	c := getNutanixCredentialsFromEnvironment()
-	creds := prismGoClient.Credentials{
-		Insecure: insecureBool,
-		Port:     nutanixPort,
-		Endpoint: nutanixEndpoint,
-		Username: c.nutanixUsername,
-		Password: c.nutanixPassword,
-	}
-
-	nutanixClient, err := nutanixClientHelper.Client(creds, nutanixClientHelper.ClientOptions{})
+	nutanixClient, err := nutanixClientHelper.Client(*creds, nutanixClientHelper.ClientOptions{})
 	if err != nil {
 		return nil, err
 	}
