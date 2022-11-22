3a1932f18dc4e0aaf9d740d435de285d9b741e78
Sid Shukla <sid.shukla@nutanix.com>
2022-11-23 00:36:24 +0530

    Update machine validation webhook
    Add checks for cluster, subnet, and image

--- api/v1beta1/nutanixmachine_webhook.go
+++ api/v1beta1/nutanixmachine_webhook.go
@@ -19,9 +19,10 @@ package v1beta1
 import (
 	"errors"
 	"fmt"
-	"k8s.io/apimachinery/pkg/api/resource"
 
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -58,32 +59,14 @@ func (r *NutanixMachine) Default() {
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-//+kubebuilder:webhook:verbs=create;update,path=/validate-infrastructure-cluster-x-k8s-io-v1beta1-nutanixmachine,mutating=false,failurePolicy=fail,groups=infrastructure.cluster.x-k8s.io,resources=nutanixmachines,versions=v1beta1,name=vnutanixmachine.kb.io,sideEffects=None,admissionReviewVersions=v1;v1beta1
+//+kubebuilder:webhook:verbs=create;update;delete,path=/validate-infrastructure-cluster-x-k8s-io-v1beta1-nutanixmachine,mutating=false,failurePolicy=fail,groups=infrastructure.cluster.x-k8s.io,resources=nutanixmachines,versions=v1beta1,name=vnutanixmachine.kb.io,sideEffects=None,admissionReviewVersions=v1;v1beta1
 
 var _ webhook.Validator = &NutanixMachine{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *NutanixMachine) ValidateCreate() error {
-	nutanixmachinelog.Info("validate create", "name", r.Name)
-	if r.Spec.VCPUSockets < minNutanixCPUSockets {
-		return errors.New(fmt.Sprintf("VCPUSockets must be greater than or equal to %d", minNutanixCPUSockets))
-	}
-	if r.Spec.VCPUsPerSocket < minNutanixCPUPerSocket {
-		return errors.New(fmt.Sprintf("VCPUPerSocket must be greater than or equal to %d", minNutanixCPUPerSocket))
-	}
-	minNutanixMemory, err := resource.ParseQuantity(fmt.Sprintf("%dMi", minNutanixMemoryMiB))
-	if err != nil {
+	if err := r.validateMachine("create"); err != nil {
 		return err
-	}
-	if r.Spec.MemorySize.Cmp(minNutanixMemory) < 0 {
-		return errors.New(fmt.Sprintf("MemoryMiB must be greater than or equal to %d", minNutanixMemoryMiB))
-	}
-	minNutanixDisk, err := resource.ParseQuantity(fmt.Sprintf("%dGi", minNutanixDiskGiB))
-	if err != nil {
-		return err
-	}
-	if r.Spec.SystemDiskSize.Cmp(minNutanixDisk) < 0 {
-		return errors.New(fmt.Sprintf("SystemDiskGiB must be greater than or equal to %d", minNutanixDiskGiB))
 	}
 
 	return nil
@@ -91,16 +74,81 @@ func (r *NutanixMachine) ValidateCreate() error {
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *NutanixMachine) ValidateUpdate(old runtime.Object) error {
-	nutanixmachinelog.Info("validate update", "name", r.Name)
+	if err := r.validateMachine("update"); err != nil {
+		return err
+	}
 
-	// TODO(user): fill in your validation logic upon object update.
 	return nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *NutanixMachine) ValidateDelete() error {
-	nutanixmachinelog.Info("validate delete", "name", r.Name)
+	if err := r.validateMachine("delete"); err != nil {
+		return err
+	}
 
-	// TODO(user): fill in your validation logic upon object deletion.
+	return nil
+}
+
+func (r *NutanixMachine) validateMachine(operation string) error {
+	nutanixmachinelog.Info("validate nutanix machine", "name", r.Name, "operation", operation)
+	if r.Spec.VCPUSockets < minNutanixCPUSockets {
+		return errors.New(fmt.Sprintf("VCPUSockets must be greater than or equal to %d", minNutanixCPUSockets))
+	}
+
+	if r.Spec.VCPUsPerSocket < minNutanixCPUPerSocket {
+		return errors.New(fmt.Sprintf("VCPUPerSocket must be greater than or equal to %d", minNutanixCPUPerSocket))
+	}
+
+	minNutanixMemory, err := resource.ParseQuantity(fmt.Sprintf("%dMi", minNutanixMemoryMiB))
+	if err != nil {
+		return err
+	}
+
+	if r.Spec.MemorySize.Cmp(minNutanixMemory) < 0 {
+		return errors.New(fmt.Sprintf("MemoryMiB must be greater than or equal to %d", minNutanixMemoryMiB))
+	}
+
+	minNutanixDisk, err := resource.ParseQuantity(fmt.Sprintf("%dGi", minNutanixDiskGiB))
+	if err != nil {
+		return err
+	}
+
+	if r.Spec.SystemDiskSize.Cmp(minNutanixDisk) < 0 {
+		return errors.New(fmt.Sprintf("SystemDiskGiB must be greater than or equal to %d", minNutanixDiskGiB))
+	}
+
+	if err := validateNutanixResourceIdentifier("cluster", r.Spec.Cluster); err != nil {
+		return err
+	}
+
+	if err := validateNutanixResourceIdentifier("image", r.Spec.Image); err != nil {
+		return err
+	}
+
+	for _, subnet := range r.Spec.Subnets {
+		if err := validateNutanixResourceIdentifier("subnet", subnet); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func validateNutanixResourceIdentifier(resource string, identifier NutanixResourceIdentifier) error {
+	parentPath := field.NewPath("providerSpec")
+	switch identifier.Type {
+	case NutanixIdentifierName:
+		if identifier.Name == nil || *identifier.Name == "" {
+			return field.Required(parentPath.Child(resource).Child("name"), fmt.Sprintf("%s name must be provided", resource))
+		}
+	case NutanixIdentifierUUID:
+		if identifier.UUID == nil || *identifier.UUID == "" {
+			return field.Required(parentPath.Child(resource).Child("uuid"), fmt.Sprintf("%s UUID must be provided", resource))
+		}
+	default:
+		return field.Invalid(parentPath.Child(resource).Child("type"), identifier.Type, fmt.Sprintf("%s type must be one of %s or %s", resource, NutanixIdentifierName, NutanixIdentifierUUID))
+	}
+
 	return nil
 }
