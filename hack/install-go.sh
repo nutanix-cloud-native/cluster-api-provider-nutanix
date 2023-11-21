d28682b71545d20be2810b0951bfafc0fe1f539a
Sid Shukla <sid.shukla@nutanix.com>
2023-11-21 18:45:19 +0530

    Add last git commit hash to build flags
    This ensures we have the git hash of the changes in our CI logs
    so we can correlate the code changes being executed.

--- hack/install-go.sh
+++ hack/install-go.sh
@@ -4,8 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-wget https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
 
