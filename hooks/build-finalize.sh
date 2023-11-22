140ba46064d34498872f425eb36d380b8021a703
deepakm-ntnx <deepak.muley@nutanix.com>
2023-11-21 16:19:56 -0800

    updated location of new layout folder

--- hooks/build-finalize.sh
+++ hooks/build-finalize.sh
@@ -18,7 +18,7 @@ set -e
 
 BUILD_DIR=~/project/package/docker
 cp -r ~/project/api $BUILD_DIR
-cp -r ~/project/controllers $BUILD_DIR
+cp -r ~/project/internal/controller $BUILD_DIR
 cp -r ~/project/pkg $BUILD_DIR
-cp go.mod go.sum main.go $BUILD_DIR
+cp go.mod go.sum cmd/main.go $BUILD_DIR
 
