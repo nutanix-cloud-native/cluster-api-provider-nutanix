df1dcb2db5b8c6ca5030832206a9473fcfc87cab
Dimitri Koshkin <dimitri.koshkin@nutanix.com>
2024-01-11 09:11:30 -0800

    fixup! refactor: client.go file helper methods

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Nutanix
+Copyright 2024 Nutanix
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
