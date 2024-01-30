d3c162dda84647b0f9c4507a44b084b793e8b584
deepakm-ntnx <deepak.muley@nutanix.com>
2024-01-30 15:24:19 -0800

    added modular tests for cluster with topology basic and upgrade workflow

---
+++ test/e2e/cluster_topology_basic_test.go
@@ -0,0 +1,166 @@
+//go:build e2e
+
+/*
+Copyright 2024 Nutanix
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+)
+
+var _ = Describe("When creating a cluster with topology with simple workflow", Label("clusterclass"), func() {
+	const specName = "cluster-with-topology-simple-workflow"
+
+	var (
+		namespace     *corev1.Namespace
+		clusterName   string
+		cluster       *clusterv1.Cluster
+		cancelWatches context.CancelFunc
+		testHelper    testHelperInterface
+	)
+
+	BeforeEach(func() {
+		testHelper = newTestHelper(e2eConfig)
+		Expect(bootstrapClusterProxy).NotTo(BeNil(), "BootstrapClusterProxy can't be nil")
+		namespace, cancelWatches = setupSpecNamespace(ctx, specName, bootstrapClusterProxy, artifactFolder)
+		Expect(namespace).NotTo(BeNil())
+	})
+
+	AfterEach(func() {
+		dumpSpecResourcesAndCleanup(ctx, specName, bootstrapClusterProxy, artifactFolder, namespace, cancelWatches, cluster, e2eConfig.GetIntervals, skipCleanup)
+	})
+
+	It("Create a cluster with topology with version Kube127", func() {
+		const flavor = "topology"
+
+		clusterName = testHelper.generateTestClusterName(specName)
+		Expect(clusterName).NotTo(BeNil())
+
+		By("Creating a workload cluster with topology")
+		nutanixE2ETest := NewNutanixE2ETest(
+			WithE2ETestSpecName(specName),
+			WithE2ETestHelper(testHelper),
+			WithE2ETestConfig(e2eConfig),
+			WithE2ETestBootstrapClusterProxy(bootstrapClusterProxy),
+			WithE2ETestArtifactFolder(artifactFolder),
+			WithE2ETestClusterctlConfigPath(clusterctlConfigPath),
+			WithE2ETestClusterTemplateFlavor(flavor),
+			WithE2ETestNamespace(namespace),
+		)
+
+		clusterTopologyConfig := NewClusterTopologyConfig(
+			WithName(clusterName),
+			WithKubernetesVersion(testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_27")),
+			WithControlPlaneCount(1),
+			WithWorkerNodeCount(1),
+			WithControlPlaneMachineTemplateImage(testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_27")),
+			WithWorkerMachineTemplateImage(testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_27")),
+		)
+
+		clusterResources, err := nutanixE2ETest.CreateCluster(ctx, clusterTopologyConfig)
+		Expect(err).ToNot(HaveOccurred())
+
+		// Set test specific variable so that cluster can be cleaned up after reach test
+		cluster = clusterResources.Cluster
+
+		By("Waiting until nodes are ready")
+		nutanixE2ETest.WaitForNodesReady(ctx, testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_27"), clusterResources)
+
+		By("PASSED!")
+	})
+
+	It("Create a cluster with topology with version Kube128", func() {
+		const flavor = "topology"
+
+		clusterName = testHelper.generateTestClusterName(specName)
+		Expect(clusterName).NotTo(BeNil())
+
+		By("Creating a workload cluster with topology")
+		nutanixE2ETest := NewNutanixE2ETest(
+			WithE2ETestHelper(testHelper),
+			WithE2ETestConfig(e2eConfig),
+			WithE2ETestBootstrapClusterProxy(bootstrapClusterProxy),
+			WithE2ETestArtifactFolder(artifactFolder),
+			WithE2ETestClusterctlConfigPath(clusterctlConfigPath),
+			WithE2ETestClusterTemplateFlavor(flavor),
+			WithE2ETestNamespace(namespace),
+		)
+
+		clusterTopologyConfig := NewClusterTopologyConfig(
+			WithName(clusterName),
+			WithKubernetesVersion(testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_28")),
+			WithControlPlaneCount(1),
+			WithWorkerNodeCount(1),
+			WithControlPlaneMachineTemplateImage(testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_28")),
+			WithWorkerMachineTemplateImage(testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_28")),
+		)
+
+		clusterResources, err := nutanixE2ETest.CreateCluster(ctx, clusterTopologyConfig)
+		Expect(err).ToNot(HaveOccurred())
+
+		// Set test specific variable so that cluster can be cleaned up after reach test
+		cluster = clusterResources.Cluster
+
+		By("Waiting until nodes are ready")
+		nutanixE2ETest.WaitForNodesReady(ctx, testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_28"), clusterResources)
+
+		By("PASSED!")
+	})
+
+	It("Create a cluster with topology with version Kube129", func() {
+		const flavor = "topology"
+
+		clusterName = testHelper.generateTestClusterName(specName)
+		Expect(clusterName).NotTo(BeNil())
+
+		By("Creating a workload cluster with topology")
+		nutanixE2ETest := NewNutanixE2ETest(
+			WithE2ETestHelper(testHelper),
+			WithE2ETestConfig(e2eConfig),
+			WithE2ETestBootstrapClusterProxy(bootstrapClusterProxy),
+			WithE2ETestArtifactFolder(artifactFolder),
+			WithE2ETestClusterctlConfigPath(clusterctlConfigPath),
+			WithE2ETestClusterTemplateFlavor(flavor),
+			WithE2ETestNamespace(namespace),
+		)
+
+		clusterTopologyConfig := NewClusterTopologyConfig(
+			WithName(clusterName),
+			WithKubernetesVersion(testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_29")),
+			WithControlPlaneCount(1),
+			WithWorkerNodeCount(1),
+			WithControlPlaneMachineTemplateImage(testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_29")),
+			WithWorkerMachineTemplateImage(testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_29")),
+		)
+
+		clusterResources, err := nutanixE2ETest.CreateCluster(ctx, clusterTopologyConfig)
+		Expect(err).ToNot(HaveOccurred())
+
+		// Set test specific variable so that cluster can be cleaned up after reach test
+		cluster = clusterResources.Cluster
+
+		By("Waiting until nodes are ready")
+		nutanixE2ETest.WaitForNodesReady(ctx, testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_29"), clusterResources)
+
+		By("PASSED!")
+	})
+})
