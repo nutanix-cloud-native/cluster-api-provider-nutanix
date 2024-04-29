c1d1fd8fb5c01db4d097d58cc6d0696c65b4d282
Deepak Muley <deepak.muley@nutanix.com>
2024-04-29 11:07:55 -0700

    added modular tests for cluster with topology for all supported k8s versions

---
+++ test/e2e/cluster_topology.go
@@ -0,0 +1,316 @@
+//go:build e2e
+
+/*
+Copyright 2024 Nutanix
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/cluster-api/test/framework"
+	"sigs.k8s.io/cluster-api/test/framework/clusterctl"
+	"sigs.k8s.io/cluster-api/test/framework/kubetest"
+)
+
+type NutanixE2ETest struct {
+	testHelper            testHelperInterface
+	e2eConfig             *clusterctl.E2EConfig
+	bootstrapClusterProxy framework.ClusterProxy
+	artifactFolder        string
+	clusterctlConfigPath  string
+	flavor                string
+	namespace             *corev1.Namespace
+	testSpecName          string
+	kubetestConfigPath    string
+}
+
+type NutanixE2ETestOption func(*NutanixE2ETest)
+
+func NewNutanixE2ETest(options ...NutanixE2ETestOption) *NutanixE2ETest {
+	nutanixE2ETest := &NutanixE2ETest{}
+	for _, o := range options {
+		o(nutanixE2ETest)
+	}
+	return nutanixE2ETest
+}
+
+func WithE2ETestSpecName(testSpecName string) NutanixE2ETestOption {
+	return func(nutanixE2ETest *NutanixE2ETest) {
+		nutanixE2ETest.testSpecName = testSpecName
+	}
+}
+
+func WithE2ETestHelper(testHelper testHelperInterface) NutanixE2ETestOption {
+	return func(nutanixE2ETest *NutanixE2ETest) {
+		nutanixE2ETest.testHelper = testHelper
+	}
+}
+
+func WithE2ETestConfig(e2eConfig *clusterctl.E2EConfig) NutanixE2ETestOption {
+	return func(nutanixE2ETest *NutanixE2ETest) {
+		nutanixE2ETest.e2eConfig = e2eConfig
+	}
+}
+
+func WithE2ETestBootstrapClusterProxy(bootstrapClusterProxy framework.ClusterProxy) NutanixE2ETestOption {
+	return func(nutanixE2ETest *NutanixE2ETest) {
+		nutanixE2ETest.bootstrapClusterProxy = bootstrapClusterProxy
+	}
+}
+
+func WithE2ETestArtifactFolder(artifactFolder string) NutanixE2ETestOption {
+	return func(nutanixE2ETest *NutanixE2ETest) {
+		nutanixE2ETest.artifactFolder = artifactFolder
+	}
+}
+
+func WithE2ETestClusterctlConfigPath(clusterctlConfigPath string) NutanixE2ETestOption {
+	return func(nutanixE2ETest *NutanixE2ETest) {
+		nutanixE2ETest.clusterctlConfigPath = clusterctlConfigPath
+	}
+}
+
+func WithE2ETestKubetestConfigPath(kubetestConfigPath string) NutanixE2ETestOption {
+	return func(nutanixE2ETest *NutanixE2ETest) {
+		nutanixE2ETest.kubetestConfigPath = kubetestConfigPath
+	}
+}
+
+func WithE2ETestClusterTemplateFlavor(flavor string) NutanixE2ETestOption {
+	return func(nutanixE2ETest *NutanixE2ETest) {
+		nutanixE2ETest.flavor = flavor
+	}
+}
+
+func WithE2ETestNamespace(namespace *corev1.Namespace) NutanixE2ETestOption {
+	return func(nutanixE2ETest *NutanixE2ETest) {
+		nutanixE2ETest.namespace = namespace
+	}
+}
+
+func (e2eTest *NutanixE2ETest) CreateCluster(ctx context.Context, clusterTopologyConfig *ClusterTopologyConfig) (*clusterctl.ApplyClusterTemplateAndWaitResult, error) {
+	configClusterInput := clusterctl.ConfigClusterInput{
+		LogFolder:                filepath.Join(e2eTest.artifactFolder, "clusters", e2eTest.bootstrapClusterProxy.GetName()),
+		ClusterctlConfigPath:     e2eTest.clusterctlConfigPath,
+		KubeconfigPath:           e2eTest.bootstrapClusterProxy.GetKubeconfigPath(),
+		InfrastructureProvider:   clusterctl.DefaultInfrastructureProvider,
+		Flavor:                   e2eTest.flavor,
+		Namespace:                e2eTest.namespace.Name,
+		ClusterName:              clusterTopologyConfig.name,
+		KubernetesVersion:        clusterTopologyConfig.k8sVersion,
+		ControlPlaneMachineCount: ptr.To(int64(clusterTopologyConfig.cpNodeCount)),
+		WorkerMachineCount:       ptr.To(int64(clusterTopologyConfig.workerNodeCount)),
+	}
+
+	clusterResources := new(clusterctl.ApplyClusterTemplateAndWaitResult)
+
+	clusterctl.ApplyClusterTemplateAndWait(ctx, clusterctl.ApplyClusterTemplateAndWaitInput{
+		ClusterProxy:                 e2eTest.bootstrapClusterProxy,
+		ConfigCluster:                configClusterInput,
+		WaitForClusterIntervals:      e2eTest.e2eConfig.GetIntervals("", "wait-cluster"),
+		WaitForControlPlaneIntervals: e2eTest.e2eConfig.GetIntervals("", "wait-control-plane"),
+		WaitForMachineDeployments:    e2eTest.e2eConfig.GetIntervals("", "wait-worker-nodes"),
+	}, clusterResources)
+
+	return clusterResources, nil
+}
+
+func (e2eTest *NutanixE2ETest) UpgradeCluster(ctx context.Context, clusterTopologyConfig *ClusterTopologyConfig) (*clusterctl.ApplyClusterTemplateAndWaitResult, error) {
+	configClusterInput := clusterctl.ConfigClusterInput{
+		LogFolder:                filepath.Join(e2eTest.artifactFolder, "clusters", e2eTest.bootstrapClusterProxy.GetName()),
+		ClusterctlConfigPath:     e2eTest.clusterctlConfigPath,
+		KubeconfigPath:           e2eTest.bootstrapClusterProxy.GetKubeconfigPath(),
+		InfrastructureProvider:   clusterctl.DefaultInfrastructureProvider,
+		Flavor:                   e2eTest.flavor,
+		Namespace:                e2eTest.namespace.Name,
+		ClusterName:              clusterTopologyConfig.name,
+		KubernetesVersion:        clusterTopologyConfig.k8sVersion,
+		ControlPlaneMachineCount: ptr.To(int64(clusterTopologyConfig.cpNodeCount)),
+		WorkerMachineCount:       ptr.To(int64(clusterTopologyConfig.workerNodeCount)),
+	}
+
+	clusterResources := new(clusterctl.ApplyClusterTemplateAndWaitResult)
+
+	clusterctl.ApplyClusterTemplateAndWait(ctx, clusterctl.ApplyClusterTemplateAndWaitInput{
+		ClusterProxy:                 e2eTest.bootstrapClusterProxy,
+		ConfigCluster:                configClusterInput,
+		WaitForClusterIntervals:      e2eTest.e2eConfig.GetIntervals("", "wait-cluster"),
+		WaitForControlPlaneIntervals: e2eTest.e2eConfig.GetIntervals("", "wait-control-plane"),
+		WaitForMachineDeployments:    e2eTest.e2eConfig.GetIntervals("", "wait-worker-nodes"),
+	}, clusterResources)
+
+	return clusterResources, nil
+}
+
+func (e2eTest *NutanixE2ETest) WaitForControlPlaneMachinesToBeUpgraded(ctx context.Context, clusterTopologyConfig *ClusterTopologyConfig, clusterResources *clusterctl.ApplyClusterTemplateAndWaitResult) {
+	waitForMachinesToBeUpgraded := e2eTest.e2eConfig.GetIntervals("", "wait-machine-upgrade")
+	mgmtClient := e2eTest.bootstrapClusterProxy.GetClient()
+	framework.WaitForControlPlaneMachinesToBeUpgraded(ctx, framework.WaitForControlPlaneMachinesToBeUpgradedInput{
+		Lister:                   mgmtClient,
+		Cluster:                  clusterResources.Cluster,
+		MachineCount:             int(clusterTopologyConfig.cpNodeCount),
+		KubernetesUpgradeVersion: clusterTopologyConfig.k8sVersion,
+	}, waitForMachinesToBeUpgraded...)
+}
+
+func (e2eTest *NutanixE2ETest) WaitForMachineDeploymentMachinesToBeUpgraded(ctx context.Context, clusterTopologyConfig *ClusterTopologyConfig, clusterResources *clusterctl.ApplyClusterTemplateAndWaitResult) {
+	waitForMachinesToBeUpgraded := e2eTest.e2eConfig.GetIntervals("", "wait-machine-upgrade")
+	mgmtClient := e2eTest.bootstrapClusterProxy.GetClient()
+	for _, deployment := range clusterResources.MachineDeployments {
+		if *deployment.Spec.Replicas > 0 {
+			framework.WaitForMachineDeploymentMachinesToBeUpgraded(ctx, framework.WaitForMachineDeploymentMachinesToBeUpgradedInput{
+				Lister:                   mgmtClient,
+				Cluster:                  clusterResources.Cluster,
+				MachineCount:             int(*deployment.Spec.Replicas),
+				KubernetesUpgradeVersion: clusterTopologyConfig.k8sVersion,
+				MachineDeployment:        *deployment,
+			}, waitForMachinesToBeUpgraded...)
+		}
+	}
+}
+
+func (e2eTest *NutanixE2ETest) WaitForNodesReady(ctx context.Context, targetKubernetesVersion string, clusterResources *clusterctl.ApplyClusterTemplateAndWaitResult) {
+	workloadProxy := e2eTest.bootstrapClusterProxy.GetWorkloadCluster(ctx, e2eTest.namespace.Name, clusterResources.Cluster.Name)
+	workloadClient := workloadProxy.GetClient()
+	framework.WaitForNodesReady(ctx, framework.WaitForNodesReadyInput{
+		Lister:            workloadClient,
+		KubernetesVersion: targetKubernetesVersion,
+		Count:             int(clusterResources.ExpectedTotalNodes()),
+		WaitForNodesReady: e2eTest.e2eConfig.GetIntervals(e2eTest.testSpecName, "wait-nodes-ready"),
+	})
+}
+
+func (e2eTest *NutanixE2ETest) RunConformanceTest(ctx context.Context, clusterResources *clusterctl.ApplyClusterTemplateAndWaitResult) error {
+	workloadProxy := e2eTest.bootstrapClusterProxy.GetWorkloadCluster(ctx, e2eTest.namespace.Name, clusterResources.Cluster.Name)
+	// Start running the conformance test suite.
+	return kubetest.Run(
+		ctx,
+		kubetest.RunInput{
+			ClusterProxy:       workloadProxy,
+			NumberOfNodes:      int(clusterResources.ExpectedWorkerNodes()),
+			ArtifactsDirectory: e2eTest.artifactFolder,
+			ConfigFilePath:     e2eTest.kubetestConfigPath,
+			GinkgoNodes:        int(clusterResources.ExpectedWorkerNodes()),
+		},
+	)
+}
+
+func (e2eTest *NutanixE2ETest) InstallAddonPackage(ctx context.Context, clusterResources *clusterctl.ApplyClusterTemplateAndWaitResult) error {
+	return fmt.Errorf("Not implemented yet.")
+}
+
+type ClusterTopologyConfig struct {
+	name                  string
+	k8sVersion            string
+	cpNodeCount           int
+	workerNodeCount       int
+	cpImageName           string
+	workerImageName       string
+	machineMemorySize     string
+	machineSystemDiskSize string
+	machineVCPUSockets    int64
+	machineVCPUSPerSocket int64
+}
+
+type ClusterTopologyConfigOption func(*ClusterTopologyConfig)
+
+func NewClusterTopologyConfig(options ...func(*ClusterTopologyConfig)) *ClusterTopologyConfig {
+	clusterTopologyConfig := &ClusterTopologyConfig{}
+	for _, o := range options {
+		o(clusterTopologyConfig)
+	}
+	return clusterTopologyConfig
+}
+
+// Start
+// Option Pattern functions for ClusterTopologyConfig
+//
+
+func WithName(name string) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.name = name
+	}
+}
+
+func WithKubernetesVersion(k8sVersion string) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.k8sVersion = k8sVersion
+	}
+}
+
+func WithControlPlaneCount(nodeCount int) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.cpNodeCount = nodeCount
+	}
+}
+
+func WithWorkerNodeCount(nodeCount int) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.workerNodeCount = nodeCount
+	}
+}
+
+func WithControlPlaneMachineTemplateImage(imageName string) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.cpImageName = imageName
+		os.Setenv("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME", imageName)
+	}
+}
+
+func WithWorkerMachineTemplateImage(imageName string) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.workerImageName = imageName
+		os.Setenv("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME", imageName)
+	}
+}
+
+func WithMachineMemorySize(machineMemorySize string) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.machineMemorySize = machineMemorySize
+		os.Setenv("NUTANIX_MACHINE_MEMORY_SIZE", machineMemorySize)
+	}
+}
+
+func WithMachineSystemDiskSize(machineSystemDiskSize string) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.machineSystemDiskSize = machineSystemDiskSize
+		os.Setenv("NUTANIX_SYSTEMDISK_SIZE", machineSystemDiskSize)
+	}
+}
+
+func WithMachineVCPUSockets(machineVCPUSockets int64) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.machineVCPUSockets = machineVCPUSockets
+		os.Setenv("NUTANIX_MACHINE_VCPU_SOCKET", fmt.Sprint(machineVCPUSockets))
+	}
+}
+
+func WithMachineVCPUSPerSocket(machineVCPUSPerSocket int64) ClusterTopologyConfigOption {
+	return func(clusterTopologyConfig *ClusterTopologyConfig) {
+		clusterTopologyConfig.machineVCPUSPerSocket = machineVCPUSPerSocket
+		os.Setenv("NUTANIX_MACHINE_VCPU_PER_SOCKET", fmt.Sprint(machineVCPUSPerSocket))
+	}
+}
+
+//
+// Option Pattern functions for ClusterTopologyConfig
+// End
---
+++ test/e2e/cluster_topology_upgrade_test.go
@@ -0,0 +1,155 @@
+//go:build e2e
+
+/*
+Copyright 2024 Nutanix
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+)
+
+var _ = Describe("When upgrading the k8s version of cluster with topology", Label("clusterclass", "only-for-validation"), func() {
+	const specName = "cluster-with-topology-upgrade-workflow"
+	const flavor = "topology"
+
+	var (
+		namespace      *corev1.Namespace
+		clusterName    string
+		cluster        *clusterv1.Cluster
+		cancelWatches  context.CancelFunc
+		testHelper     testHelperInterface
+		nutanixE2ETest *NutanixE2ETest
+	)
+
+	BeforeEach(func() {
+		testHelper = newTestHelper(e2eConfig)
+		Expect(bootstrapClusterProxy).NotTo(BeNil(), "BootstrapClusterProxy can't be nil")
+		namespace, cancelWatches = setupSpecNamespace(ctx, specName, bootstrapClusterProxy, artifactFolder)
+		Expect(namespace).NotTo(BeNil())
+		nutanixE2ETest = NewNutanixE2ETest(
+			WithE2ETestSpecName(specName),
+			WithE2ETestHelper(testHelper),
+			WithE2ETestConfig(e2eConfig),
+			WithE2ETestBootstrapClusterProxy(bootstrapClusterProxy),
+			WithE2ETestArtifactFolder(artifactFolder),
+			WithE2ETestClusterctlConfigPath(clusterctlConfigPath),
+			WithE2ETestClusterTemplateFlavor(flavor),
+			WithE2ETestNamespace(namespace),
+		)
+	})
+
+	AfterEach(func() {
+		dumpSpecResourcesAndCleanup(ctx, specName, bootstrapClusterProxy, artifactFolder, namespace, cancelWatches, cluster, e2eConfig.GetIntervals, skipCleanup)
+	})
+
+	upgradeWorkflow := func(fromKubeVer, fromImageName, toKubeVer, toImageName string) {
+		By("Creating a workload cluster with topology")
+		clusterTopologyConfig := NewClusterTopologyConfig(
+			WithName(clusterName),
+			WithKubernetesVersion(fromKubeVer),
+			WithControlPlaneCount(1),
+			WithWorkerNodeCount(1),
+			WithControlPlaneMachineTemplateImage(fromImageName),
+			WithWorkerMachineTemplateImage(fromImageName),
+		)
+
+		clusterResources, err := nutanixE2ETest.CreateCluster(ctx, clusterTopologyConfig)
+		Expect(err).ToNot(HaveOccurred())
+
+		// Set test specific variable so that cluster can be cleaned up after each test
+		cluster = clusterResources.Cluster
+
+		By("Waiting until nodes are ready")
+		nutanixE2ETest.WaitForNodesReady(ctx, fromKubeVer, clusterResources)
+
+		clusterTopologyConfig = NewClusterTopologyConfig(
+			WithName(clusterName),
+			WithKubernetesVersion(toKubeVer),
+			WithControlPlaneCount(1),
+			WithWorkerNodeCount(1),
+			WithControlPlaneMachineTemplateImage(toImageName),
+			WithWorkerMachineTemplateImage(toImageName),
+		)
+
+		clusterResources, err = nutanixE2ETest.UpgradeCluster(ctx, clusterTopologyConfig)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Waiting for control-plane machines to have the upgraded Kubernetes version")
+		nutanixE2ETest.WaitForControlPlaneMachinesToBeUpgraded(ctx, clusterTopologyConfig, clusterResources)
+
+		By("Waiting for machine deployment machines to have the upgraded Kubernetes version")
+		nutanixE2ETest.WaitForMachineDeploymentMachinesToBeUpgraded(ctx, clusterTopologyConfig, clusterResources)
+
+		By("Waiting until nodes are ready")
+		nutanixE2ETest.WaitForNodesReady(ctx, toKubeVer, clusterResources)
+
+		By("PASSED!")
+	}
+
+	It("Upgrade a cluster with topology from version Kube126 to Kube127", Label("Kube127", "cluster-topology-upgrade"), func() {
+		clusterName = testHelper.generateTestClusterName(specName)
+		Expect(clusterName).NotTo(BeNil())
+
+		kube126 := testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_26")
+		kube126Image := testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_26")
+		kube127 := testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_27")
+		kube127Image := testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_27")
+		upgradeWorkflow(kube126, kube126Image, kube127, kube127Image)
+	})
+
+	It("Upgrade a cluster with topology from version Kube127 to Kube128", Label("Kube128", "cluster-topology-upgrade"), func() {
+		clusterName = testHelper.generateTestClusterName(specName)
+		Expect(clusterName).NotTo(BeNil())
+
+		kube127 := testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_27")
+		kube127Image := testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_27")
+		kube128 := testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_28")
+		kube128Image := testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_28")
+		upgradeWorkflow(kube127, kube127Image, kube128, kube128Image)
+	})
+
+	// It("Upgrade a cluster with topology from version Kube126 to Kube128 and expect failure", Label("Kube128", "cluster-topology-upgrade-test"), func() {
+	// 	// NOTE: Following test will always fail and did not find a way to not fail as function we call has Expect call
+	// 	// For more info on why refer https://cluster-api.sigs.k8s.io/tasks/experimental-features/cluster-class/operate-cluster
+	// 	// we get following error
+	// 	// The Cluster "cluster-with-topology-upgrade-workflow-u318rg" is invalid: spec.topology.version: Forbidden: version cannot be increased from "1.26.13" to "1.28.6"
+	// 	clusterName = testHelper.generateTestClusterName(specName)
+	// 	Expect(clusterName).NotTo(BeNil())
+
+	// 	kube126 := testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_26")
+	// 	kube126Image := testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_26")
+	// 	kube128 := testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_28")
+	// 	kube128Image := testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_28")
+	// 	upgradeWorkflow(kube126, kube126Image, kube128, kube128Image)
+	// })
+
+	It("Upgrade a cluster with topology from version Kube128 to Kube129", Label("Kube129", "cluster-topology-upgrade"), func() {
+		clusterName = testHelper.generateTestClusterName(specName)
+		Expect(clusterName).NotTo(BeNil())
+
+		kube128 := testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_28")
+		kube128Image := testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_28")
+		kube129 := testHelper.getVariableFromE2eConfig("KUBERNETES_VERSION_v1_29")
+		kube129Image := testHelper.getVariableFromE2eConfig("NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME_v1_29")
+		upgradeWorkflow(kube128, kube128Image, kube129, kube129Image)
+	})
+})
