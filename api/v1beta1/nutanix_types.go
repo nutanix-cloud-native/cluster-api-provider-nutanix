1c1d4f9e06b1910cda859e08670ca00233fe4fc8
Abhay Aggrawal <abhay.aggrawal@nutanix.com>
2025-08-22 10:43:34 +0530

    Update api/v1beta1/nutanix_types.go
    Co-authored-by: Jimmi Dyson <jimmi.dyson@nutanix.com>

--- api/v1beta1/nutanix_types.go
+++ api/v1beta1/nutanix_types.go
@@ -61,7 +61,10 @@ const (
 
 // NutanixResourceIdentifier holds the identity of a Nutanix PC resource (cluster, image, subnet, etc.)
 // +union
-// +kubebuilder:validation:XValidation:rule="self.type == 'name' ? (has(self.name) && size(self.name) > 0) : !has(self.name)",message="'name' must be set and non-empty when type is 'name', and forbidden otherwise"
+// +kubebuilder:validation:XValidation:rule="self.type != 'name' || has(self.name)",message="the 'name' field must be present when the 'type' is 'name'"
+// +kubebuilder:validation:XValidation:rule="self.type != 'uuid' || has(self.uuid)",message="'the 'uuid' field must be present when the 'type' is 'uuid'"
+// +kubebuilder:validation:XValidation:rule="self.type != 'name' || !has(self.uuid)",message="the 'uuid' field cannot be set when the 'type' is 'name'"
+// +kubebuilder:validation:XValidation:rule="self.type == 'uuid' && !has(self.name)",message="the 'name' field cannot be set when the 'type' is 'uuid'"
 // +kubebuilder:validation:XValidation:rule="self.type == 'uuid' ? has(self.uuid) && self.uuid.contains('-') : !has(self.uuid)",message="'uuid' configuration is required when type is 'uuid', and forbidden otherwise"
 type NutanixResourceIdentifier struct {
 	// Type is the identifier type to use for this resource.
