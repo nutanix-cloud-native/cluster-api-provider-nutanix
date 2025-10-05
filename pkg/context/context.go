ae84e02f39716b619e31039ecad2aaaacb0e75a1
Abhay Aggrawal <abhay.aggrawal@nutanix.com>
2025-10-05 17:21:08 +0530

    refactor: removing prism-go-client/utils (#586)

--- pkg/context/context.go
+++ pkg/context/context.go
@@ -21,8 +21,8 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/nutanix-cloud-native/prism-go-client/utils"
 	prismclientv3 "github.com/nutanix-cloud-native/prism-go-client/v3"
+	"k8s.io/utils/ptr"
 	capiv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/cluster-api/controllers/remote"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -93,14 +93,14 @@ func (clctx *ClusterContext) GetNutanixMachinesInCluster(client ctlclient.Client
 func (clctx *ClusterContext) SetFailureStatus(failureReason string, failureMessage error) {
 	log := ctrl.LoggerFrom(clctx.Context)
 	log.Error(failureMessage, fmt.Sprintf("cluster failed: %s", failureReason))
-	clctx.NutanixCluster.Status.FailureMessage = utils.StringPtr(fmt.Sprintf("%v", failureMessage))
+	clctx.NutanixCluster.Status.FailureMessage = ptr.To(fmt.Sprintf("%v", failureMessage))
 	clctx.NutanixCluster.Status.FailureReason = &failureReason
 }
 
 func (clctx *MachineContext) SetFailureStatus(failureReason string, failureMessage error) {
 	log := ctrl.LoggerFrom(clctx.Context)
 	log.Error(failureMessage, fmt.Sprintf("machine failed: %s", failureReason))
-	clctx.NutanixMachine.Status.FailureMessage = utils.StringPtr(fmt.Sprintf("%v", failureMessage))
+	clctx.NutanixMachine.Status.FailureMessage = ptr.To(fmt.Sprintf("%v", failureMessage))
 	clctx.NutanixMachine.Status.FailureReason = &failureReason
 }
 
