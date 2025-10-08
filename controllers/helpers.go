d50b67446052089a3ef8fd3e3eb8bcd53502497f
Ilya Alekseyev <ilya.alekseyev@nutanix.com>
2025-10-08 18:27:18 +0000

    Restore ImageLookup original behaviour

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -493,54 +493,28 @@ func GetImageByLookup(
 			err,
 		)
 	}
-
-	imagePrefix := makeImagePrefix(t, params)
-	responseImages, err := client.Images.List(ctx, converged.WithFilter(fmt.Sprintf("startwith(name, '%s')", imagePrefix)))
+	responseImages, err := client.Images.List(ctx)
 	if err != nil {
 		return nil, err
 	}
-
-	// TODO: @adiantum:We need to improve this, leaving as it is for now to avoid breaking changes.
-	imagePattern := templateBytes.String()
-	re := regexp.MustCompile(imagePattern)
-	foundImages := make([]imageModels.Image, 0)
+	re := regexp.MustCompile(templateBytes.String())
+	foundImages := make([]*imageModels.Image, 0)
 	for _, image := range responseImages {
 		if re.Match([]byte(*image.Name)) {
-			foundImages = append(foundImages, image)
+			foundImages = append(foundImages, &image)
 		}
 	}
 	sorted := sortImagesByLatestCreationTime(foundImages)
 	if len(sorted) == 0 {
-		return nil, fmt.Errorf("failed to find image with filter '%s'", imagePattern)
+		return nil, fmt.Errorf("failed to find image with filter %s", templateBytes.String())
 	}
-	return &sorted[0], nil
-}
-
-func makeImagePrefix(t *template.Template, params ImageLookup) string {
-	var templateBytes bytes.Buffer
-	delimiter := uuid.New().String()
-
-	if params.BaseOS == "" {
-		params.BaseOS = delimiter
-	}
-
-	if params.K8sVersion == "" {
-		params.K8sVersion = delimiter
-	}
-
-	err := t.Execute(&templateBytes, params)
-	if err != nil {
-		return ""
-	}
-
-	parts := strings.Split(templateBytes.String(), delimiter)
-	return parts[0]
+	return sorted[0], nil
 }
 
 // returns the images with the latest creation time first.
 func sortImagesByLatestCreationTime(
-	images []imageModels.Image,
-) []imageModels.Image {
+	images []*imageModels.Image,
+) []*imageModels.Image {
 	sort.Slice(images, func(i, j int) bool {
 		if images[i].CreateTime == nil || images[j].CreateTime == nil {
 			return images[i].CreateTime != nil
