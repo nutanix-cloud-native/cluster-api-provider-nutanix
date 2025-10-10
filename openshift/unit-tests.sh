f7f901bde9a1e588f6bb5852e2bbcd7d74f400b8
Damiano Donati <damiano.donati@gmail.com>
2025-10-10 14:58:47 +0200

    UPSTREAM: <carry>: add mockgen/setup-envtest installation to openshift/unit-tests.sh

--- openshift/unit-tests.sh
+++ openshift/unit-tests.sh
@@ -11,4 +11,15 @@ if [ $HOME == "/" ]; then
   export HOME=/tmp/kubebuilder/testing
 fi
 
+# mockgen is implicitly available in the nutanix-cloud-native/cluster-api-provider-nutanix CI environment.
+# context: https://github.com/nutanix-cloud-native/cluster-api-provider-nutanix/pull/612#issuecomment-3389742494
+MOCKGEN_VERSION="v0.4.0"
+echo "Installing mockgen@${MOCKGEN_VERSION}"
+go install -mod=mod "go.uber.org/mock/mockgen@${MOCKGEN_VERSION}"
+
+# setup-envtest is implicitly available in the nutanix-cloud-native/cluster-api-provider-nutanix CI environment.
+SETUP_ENVTEST_VERSION="release-0.16"
+echo "Installing setup-envtest@${SETUP_ENVTEST_VERSION}"
+go install -mod=mod "sigs.k8s.io/controller-runtime/tools/setup-envtest@${SETUP_ENVTEST_VERSION}"
+
 GOFLAGS='-mod=readonly' make unit-test
