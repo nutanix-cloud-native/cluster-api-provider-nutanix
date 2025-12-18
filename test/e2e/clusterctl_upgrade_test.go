9fa18a152c0fa19dbc3366d56a8d562931875d98
Atul Verma <atul.verma@nutanix.com>
2025-12-18 12:38:23 +0000

    test(e2e): bump capx to 1.8.2 for clusterctl upgrade

--- test/e2e/clusterctl_upgrade_test.go
+++ test/e2e/clusterctl_upgrade_test.go
@@ -42,7 +42,7 @@ import (
 	"github.com/nutanix-cloud-native/cluster-api-provider-nutanix/test/e2e/log"
 )
 
-var _ = Describe("clusterctl upgrade CAPX (v1.7.1 => current)", Label("clusterctl-upgrade"), func() {
+var _ = Describe("clusterctl upgrade CAPX (v1.8.3 => current)", Label("clusterctl-upgrade"), func() {
 	var (
 		kubernetesVersion                      string
 		nutanixMachineTemplateImageName        string
@@ -95,7 +95,7 @@ var _ = Describe("clusterctl upgrade CAPX (v1.7.1 => current)", Label("clusterct
 			InitWithCoreProvider:            "cluster-api:v1.11.4",
 			InitWithBootstrapProviders:      []string{"kubeadm:v1.11.4"},
 			InitWithControlPlaneProviders:   []string{"kubeadm:v1.11.4"},
-			InitWithInfrastructureProviders: []string{"nutanix:v1.7.1"},
+			InitWithInfrastructureProviders: []string{"nutanix:v1.8.3"},
 			PreWaitForCluster:               preWaitForCluster,
 			PostUpgrade:                     postUpgradeFunc,
 		}
@@ -111,7 +111,7 @@ func createPreWaitForClusterFunc(testInputFunc func() capie2e.ClusterctlUpgradeS
 
 		By("Get latest version of CAPX provider")
 
-		latestVersionString := "v1.7.1"
+		latestVersionString := "v1.8.3"
 		latestVersion, err := semver.ParseTolerant(latestVersionString)
 		Expect(err).NotTo(HaveOccurred())
 
@@ -170,7 +170,7 @@ func createPostUpgradeFunc(testInputFunc func() capie2e.ClusterctlUpgradeSpecInp
 
 		yamlProc := yaml.NewSimpleProcessor()
 
-		latestVersionString := "v1.7.1"
+		latestVersionString := "v1.8.3"
 		latestVersion, err := semver.ParseTolerant(latestVersionString)
 		Expect(err).NotTo(HaveOccurred())
 
